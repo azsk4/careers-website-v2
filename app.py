6233a4644112ba35157933c352c7c12afe9c049a
azsk4 <55152716+azsk4@users.noreply.github.com>
2023-08-01 14:46:28 +0000

    added connection to db

--- app.py
+++ app.py
@@ -1,42 +1,18 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)  # created a flask app
 
-JOBS = [
-  {
-    'id':1,
-    'title':'Data Analyst',
-    'location':'London, England',
-    'salary':'£30,000'
-  },
-  {
-    'id':2,
-    'title':'Data Scientist',
-    'location':'Manchester, England',
-    'salary':'£30,000'
-  },
-  {
-    'id':3,
-    'title':'Frontend Engineer',
-    'location':'Remote',
-    'salary':'£30,000'
-  },
-  {
-    'id':4,
-    'title':'Backend Engineer',
-    'location':'San Francisco, USA',
-    'salary':'$40,000'
-  },
-]
-
 @app.route("/")  # registered a route to the app 
 def hello_world():
-  return render_template('home.html', jobs=JOBS, company_name='Khan')
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name='Khan')
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 if __name__ == "__main__":  # checked if we are runnning the app.py file
-  app.run(host='0.0.0.0', debug=True)  # start the app using app.run
+  app.run(host='0.0.0.0', port = 4444, debug=True)  # start the app using app.run
   
