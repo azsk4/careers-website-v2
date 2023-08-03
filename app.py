fd3d41596a3658103bbd1809179f5019b4250e6e
azsk4 <55152716+azsk4@users.noreply.github.com>
2023-08-03 16:42:43 +0000

    dynamic pages for each job

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)  # created a flask app
 
 @app.route("/")  # registered a route to the app 
 def hello_world():
   jobs = load_jobs_from_db()
-  return render_template('home.html', jobs=jobs, company_name='Khan')
+  return render_template('home.html', jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
 
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+    
+  return render_template('jobpage.html', 
+                         job=job)
+
 if __name__ == "__main__":  # checked if we are runnning the app.py file
-  app.run(host='0.0.0.0', port = 4444, debug=True)  # start the app using app.run
+  app.run(host='0.0.0.0', port = 8080, debug=True)  # start the app using app.run
   
--- database.py
+++ database.py
@@ -17,3 +17,20 @@ def load_jobs_from_db():
     for row in result.all():
       jobs.append(dict(row._mapping))
     return jobs
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      {"val":id}  # string formatting (:val --> val = id)
+    )
+    row = result.fetchone()
+    return row._asdict()
+
+    '''rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])'''
+    
+    
